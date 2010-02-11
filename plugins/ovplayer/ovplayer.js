e6b6844d5e0410f8de5c3ed74cac90e66c0e316f
Cary Lee <carylee@gmail.com>
2010-02-11 16:18:59 -0600

    Added javascript parsing of height/width options

--- plugins/ovplayer/ovplayer.js
+++ plugins/ovplayer/ovplayer.js
@@ -20,9 +20,19 @@ Drupal.wysiwyg.plugins['ovplayer'] = {
       var optionString = "\"dialogWidth:" + settings.dialog.width + "px;dialogHeight:" + settings.dialog.height + "px;\"";
       var videoValue = window.showModalDialog(settings.dialog.url, "", optionString);
       var videoUrl = videoValue[0];
+      var videoHeight = videoValue[1];
+      var videoWidth = videoValue[2];
 
       if( videoValue ) {
-        var ovpString = "[ovp:" + videoUrl + "]";
+        var ovpString = "[ovp:";
+        if( videoHeight ) {
+          ovpString += "height=" + videoHeight + ",";
+        }
+        if( videoWidth ) {
+          ovpString += "width=" + videoWidth + ",";
+        }
+
+        ovpString += videoUrl + "]";
         Drupal.wysiwyg.instances[instanceId].insert(ovpString);
       }
   },
