262b16838e084282747fc92efd70cac58823f020
Cary Lee <carylee@gmail.com>
2010-02-12 09:24:28 -0600

    Commented out unused functions

--- plugins/ovplayer/ovplayer.js
+++ plugins/ovplayer/ovplayer.js
@@ -5,9 +5,9 @@ Drupal.wysiwyg.plugins['ovplayer'] = {
   /**
    * Return whether the passed node belongs to this plugin.
    */
-  isNode: function(node) {
+  /*isNode: function(node) {
     return ($(node).is('img.wysiwyg-ovplayer'));
-  },
+  },*/
 
   /**
    * Execute the button.
@@ -40,15 +40,15 @@ Drupal.wysiwyg.plugins['ovplayer'] = {
   /**
    * Replace all <!--column--> tags with images.
    */
-  attach: function(content, settings, instanceId) {
+  /*attach: function(content, settings, instanceId) {
     content = content.replace(/<!--column-->/g, this._getPlaceholder(settings));
     return content;
-  },
+  },*/
 
   /**
    * Replace images with <!--column--> tags in content upon detaching editor.
    */
-  detach: function(content, settings, instanceId) {
+  /*detach: function(content, settings, instanceId) {
     var $content = $('<div>' + content + '</div>'); // No .outerHTML() in jQuery :(
     // #404532: document.createComment() required or IE will strip the comment.
     // #474908: IE 8 breaks when using jQuery methods to replace the elements.
@@ -58,7 +58,7 @@ Drupal.wysiwyg.plugins['ovplayer'] = {
       elem.parentNode.removeChild(elem);
     });
     return $content.html();
-  },
+  },*/
 
   /**
    * Helper function to return a HTML placeholder.
